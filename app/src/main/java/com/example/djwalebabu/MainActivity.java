96a7d0208617ab8d8e5b7ba9086eeb59794ac455
seema kale <seemakale216@gmail.coma>
2019-09-07 17:04:09 -0700

    Initial commit

---
+++ app/src/main/java/com/example/djwalebabu/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.djwalebabu;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Random;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, View.OnTouchListener, View.OnContextClickListener {
+    MediaPlayer mediaPlayer1,mediaPlayer2;
+    Random random=new Random();
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        View view=new View(this);
+        view.setOnContextClickListener(this);
+        view.setOnLongClickListener(this);
+        view.setOnTouchListener(this);
+        setContentView(view);
+        mediaPlayer1=MediaPlayer.create(this,R.raw.backgroundmusic);
+        mediaPlayer2=MediaPlayer.create(this,R.raw.soundtrack);
+        view.setBackgroundColor(Color.rgb(random.nextInt(268),random.nextInt(265),random.nextInt(286)));
+
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        mediaPlayer2.start();
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onContextClick(View view) {
+        mediaPlayer1.start();
+        return false;
+    }
+}
