5f04d4819c9af1d64c02708119a10b4754e09c23
seema kale <seemakale216@gmail.coma>
2019-09-07 17:18:55 -0700

    done

--- app/src/main/java/com/example/djwalebabu/MainActivity.java
+++ app/src/main/java/com/example/djwalebabu/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -15,7 +17,12 @@ import java.util.Random;
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, View.OnTouchListener, View.OnContextClickListener {
     MediaPlayer mediaPlayer1,mediaPlayer2;
+    //for random number generation
     Random random=new Random();
+
+    //For small and light music
+    SoundPool soundPool;
+    int dm=0;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -27,6 +34,12 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         view.setOnLongClickListener(this);
         view.setOnTouchListener(this);
         setContentView(view);
+
+        //soundPool
+        soundPool=new SoundPool(5, AudioManager.STREAM_MUSIC,0);
+        //allocate resources to soundpool
+        dm=soundPool.load(this,R.raw.explosion,1);
+
         mediaPlayer1=MediaPlayer.create(this,R.raw.backgroundmusic);
         mediaPlayer2=MediaPlayer.create(this,R.raw.soundtrack);
         view.setBackgroundColor(Color.rgb(random.nextInt(268),random.nextInt(265),random.nextInt(286)));
@@ -41,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (dm!=0){
+            soundPool.play(dm,1,1,0,0,1);
+        }
         return false;
     }
 
